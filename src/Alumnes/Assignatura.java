82bb5e1be7ac2706c58c09fb54885d0e32ead3a3
Alvarooo9 <agarcialo@edu.tecnocampus.cat>
2024-11-06 09:45:36 +0100

    Class Assignatura

--- src/Alumnes/Assignatura.java
+++ src/Alumnes/Assignatura.java
@@ -1,4 +1,19 @@
 package Alumnes;
 
 public class Assignatura {
+
+    public static final int EXCELLENT = 9;
+    public static final int NOTABLE = 7;
+    public static final int APROVAT = 5;
+
+    private final String nom;
+    private final int credits;
+    private double nota;
+    private final boolean mHonor;
+
+    public Assignatura(String nom, int credits, double nota, boolean mHonor){
+
+    }
+
+
 }
