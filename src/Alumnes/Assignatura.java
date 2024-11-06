51fed3b0c30150c4028bfa2eb3843f33f965385f
Alvarooo9 <agarcialo@edu.tecnocampus.cat>
2024-11-06 11:41:31 +0100

    Assignatura finished

--- src/Alumnes/Assignatura.java
+++ src/Alumnes/Assignatura.java
@@ -13,19 +13,58 @@ public class Assignatura {
     private final boolean mHonor;
 
     public Assignatura(String nom, int credits, double nota, boolean mHonor) {
+
         if (credits < 0 || nota < 0) throw new IllegalArgumentException();
-        if (nota >= EXCELLENT && mHonor==true){
-            mHonor = true;
-        }
         this.nom = nom;
         this.credits = credits;
         this.nota = nota;
-        this.mHonor = mHonor;
-
+        this.mHonor = (nota >= EXCELLENT) && mHonor;
     }
 
+    public Assignatura(String nom) {
+        this.nom = nom;
+        this.credits = 0;
+        this.nota = 0.0;
+        this.mHonor = false;
+    }
 
+    public void setNota(double nota) {
+        if (nota < 0) {
+            throw new IllegalArgumentException("La nota no pot ser negativa.");
+        }
+        this.nota = nota;
+    }
 
+    public double getNota() {
+        return this.nota;
+    }
 
+    public int getPunts (){
+        if (nota >= EXCELLENT) {
+            return mHonor ? 4 : 3;
+        } else if (nota >= NOTABLE) {
+            return 2;
+        } else if (nota >= APROVAT) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 
+    public int getCredits() {
+        return this.credits;
+    }
+
+    @Override
+    public String toString() {
+        return "Assignatura: " + nom + ", Nota mitjana: " + nota;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Assignatura that = (Assignatura) obj;
+        return nom.equals(that.nom);
+    }
 }
