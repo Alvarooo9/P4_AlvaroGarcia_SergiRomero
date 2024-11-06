9ebf79daaa85172c7d39655c0a82ad900d1dd392
Alvarooo9 <agarcialo@edu.tecnocampus.cat>
2024-11-06 09:59:56 +0100

    Class Assignatura

--- src/Alumnes/Assignatura.java
+++ src/Alumnes/Assignatura.java
@@ -7,11 +7,24 @@ public class Assignatura {
     public static final int NOTABLE = 7;
     public static final int APROVAT = 5;
 
+    private final String nom;
+    private final int credits;
+    private double nota;
+    private final boolean mHonor;
 
-
-    public Assignatura(String nom, int credits, double nota, boolean mHonor){
+    public Assignatura(String nom, int credits, double nota, boolean mHonor) {
+        if (credits < 0 || nota < 0) throw new IllegalArgumentException();
+        if (nota >= EXCELLENT && mHonor==true){
+            mHonor = true;
+        }
+        this.nom = nom;
+        this.credits = credits;
+        this.nota = nota;
+        this.mHonor = mHonor;
 
     }
+
+
 
 
 
